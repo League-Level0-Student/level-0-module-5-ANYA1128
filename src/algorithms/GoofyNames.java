5b18c91f66327a271f1103ad3c2488dadb8c047f
League <League@adminisorsimac2.attlocal.net>
2018-08-12 12:59:13 -0700

    Finished CircleRing, GoofyNames, and GuessingPi and all the quizzes that go with them. Start ForLoopGauntlet next.

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -18,17 +18,25 @@ public class GoofyNames {
 
 		// 3. Loop through each character of the name (steps 4 - 6).
 		// HINT: Use .length() to determine the number of characters in the String.
+		for (int i = 0; i < name.length(); i++) {
+			// 4. Create a String variable to store the next character of the name
+			String nxtChar = name.substring(i, i+1);
+			// using .substring(start, end)
+			// HINT: replace 'start' and 'end' to get String with character at i
 
-		// 4. Create a String variable to store the next character of the name
-		// using .substring(start, end)
-		// HINT: replace 'start' and 'end' to get String with character at i
-
-		// 5. Use MODULO to set this variable to upper case for EVEN characters
-		// and lower case for ODD characters.
-
-		// 6. ADD this String (containing 1 char) to the goofyName String
-
-		// 7. Use pop-up to show user their Goofy name
+			// 5. Use MODULO to set this variable to upper case for EVEN characters
+			// and lower case for ODD characters.
+               if (i%2==0 ) {
+            	 nxtChar = nxtChar.toUpperCase();
+               }
+               else if (i%2!=0 ) {
+              	 nxtChar = nxtChar.toLowerCase();
+                 }
+			// 6. ADD this String (containing 1 char) to the goofyName String
+        goofyName = nxtChar;
+			// 7. Use pop-up to show user their Goofy name
+          JOptionPane.showMessageDialog(null, goofyName);
+		}
 
 	}
 }
