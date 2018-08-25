294ac53d99866107262901be29bd47f7e50bc3b5
blackbeltcam <league@imac.attlocal.net>
2018-08-24 18:25:37 -0700

    SkillPracticeWorksheet, Code4Life

--- src/extra/Code4Life.java
+++ src/extra/Code4Life.java
@@ -11,27 +11,38 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
-
+import javax.swing.JOptionPane;
 public class Code4Life {
-
+public static void main(String[] args) {
 	// 1. Ask the user how many hours they spent coding this week.
-
-	// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
-	// write code instead.
-
-	// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
-	// they're a Code Ninja
-
-	// 4. If it is more than 5, call the method below to play the Batman theme song.
-
-	private static void playBatmanTheme() {
-		try {
-			AudioClip sound = JApplet.newAudioClip(Code4Life.class.getResource("batman.wav"));
-			sound.play();
-			Thread.sleep(60002);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
-
+	String time = JOptionPane.showInputDialog(null, "How many hours did you spend coding this week?");
+	int hours = Integer.parseInt(time);
+		// 2. If it is less than or equal to 2, tell them to stop watching YouTube and
+		// write code instead.
+if (hours <= 2) {
+	JOptionPane.showMessageDialog(null, "STOP WATCHING YOUTUBE!!! and write code instead :)");
 }
+		// 3. If it is greater than or equal to 3 AND less than or equal to 5, tell them
+		// they're a Code Ninja
+ if (hours >= 3) {
+	JOptionPane.showMessageDialog(null, "Congrats. You are a coding ninja.");
+}
+	
+		// 4. If it is more than 5, call the method below to play the Batman theme song.
+ if (hours > 5) {
+	playBatmanTheme();
+}
+		
+
+	}	
+private static void playBatmanTheme() {
+	try {
+		AudioClip sound = JApplet.newAudioClip(Code4Life.class.getResource("batman.wav"));
+		sound.play();
+		Thread.sleep(60002);
+	} catch (Exception ex) {
+		ex.printStackTrace();
+	}
+}
+}
+	
